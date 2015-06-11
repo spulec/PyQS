e0b930a11d3e3aefc100ee7bb27dccc0742f9e03
Andrew Gross <andrew.w.gross@gmail.com>
2015-06-11 15:11:27 -0400

    Sleep for 1ms instead of 10ms

--- pyqs/worker.py
+++ pyqs/worker.py
@@ -174,7 +174,7 @@ class ManagerWorker(object):
     def sleep(self):
         try:
             while True:
-                time.sleep(0.01)
+                time.sleep(0.001)
         except KeyboardInterrupt:
             print('\n')
             print('Graceful shutdown...')
