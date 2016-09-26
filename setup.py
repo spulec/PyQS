ee229335e3a377e6c8759f80437198f58709cb5f
Adam Chainz <adam@adamj.eu>
2016-09-26 17:30:57 +0100

    Remove requirements.txt in favour of plain install_requires
    There's only one requirement, so the code in `setup.py` is a bit heavyweight. Writing plain `install_requires` in the `setup.py` is more straightforward.

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import os
 import re
 
 from setuptools import setup, find_packages
 
-version = ''
 with open('pyqs/__init__.py', 'r') as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
@@ -15,40 +13,6 @@ with open('pyqs/__init__.py', 'r') as fd:
 if not version:
     raise RuntimeError('Cannot find version information')
 
-
-def parse_requirements():
-    """Rudimentary parser for the `requirements.txt` file
-
-    We just want to separate regular packages from links to pass them to the
-    `install_requires` and `dependency_links` params of the `setup()`
-    function properly.
-    """
-    try:
-        requirements = \
-            map(str.strip, local_file('requirements.txt').splitlines())
-    except IOError:
-        raise RuntimeError("Couldn't find the `requirements.txt' file :(")
-
-    links = []
-    pkgs = []
-    for req in requirements:
-        if not req:
-            continue
-        if 'http:' in req or 'https:' in req:
-            links.append(req)
-            name, version = re.findall("\#egg=([^\-]+)-(.+$)", req)[0]
-            pkgs.append('{0}=={1}'.format(name, version))
-        else:
-            pkgs.append(req)
-
-    return pkgs, links
-
-
-local_file = lambda f: \
-    open(os.path.join(os.path.dirname(__file__), f)).read()
-
-
-install_requires, dependency_links = parse_requirements()
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -69,8 +33,9 @@ setup(
             'pyqs = pyqs.main:main',
         ],
     },
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+    install_requires=[
+        'boto>=2.32.1'
+    ],
     packages=filter(lambda n: not n.startswith('tests'), find_packages()),
     include_package_data=True,
 )
