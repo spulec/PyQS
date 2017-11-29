e014ec75d9d7a4ba681ef37bf63eb2224bfa865a
Andrew Gross <andrew.w.gross@gmail.com>
2017-11-29 15:48:09 -0500

    Revert "Hope we can reconcile the 2/3 threading interfaces"
    This reverts commit 7b6dd0b451264f677c02f6cc5f52812c82846726.

--- tests/utils.py
+++ tests/utils.py
@@ -27,8 +27,8 @@ class MockLoggingHandler(logging.Handler):
 
 
 class ThreadWithReturnValue(Thread):
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}):
-        Thread.__init__(self, group, target, name, args, kwargs)
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs, Verbose)
         self._return = None
 
     def run(self):
