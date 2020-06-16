f557af1c6078cc93dcc05e194017eea86adfc0b9
Jim Shields <jim.j.shields@gmail.com>
2020-06-16 12:24:42 -0400

    Clean up events API & add unit tests

--- pyqs/events.py
+++ pyqs/events.py
@@ -1,19 +1,41 @@
+"""
+pyqs events registry: register callback functions on pyqs events
+
+Usage:
+from pyqs.events import register_event
+
+register_event("pre_process", lambda context: print(context))
+"""
+
+
 class Events:
     def __init__(self):
         self.pre_process = []
         self.post_process = []
 
+    def clear(self):
+        self.pre_process = []
+        self.post_process = []
 
-# Singleton
-EVENTS = Events()
+
+# Global singleton
+_EVENTS = Events()
 
 
-def register(name, callback):
-    if hasattr(EVENTS, name):
-        getattr(EVENTS, name).append(callback)
+class NoEventException(Exception):
+    pass
+
+
+def register_event(name, callback):
+    if hasattr(_EVENTS, name):
+        getattr(_EVENTS, name).append(callback)
     else:
-        raise Exception(f"{name} is not a valid pyqs event.")
+        raise NoEventException(f"{name} is not a valid pyqs event.")
 
 
 def get_events():
-    return EVENTS
+    return _EVENTS
+
+
+def clear_events():
+    _EVENTS.clear()
---
+++ tests/test_events.py
@@ -0,0 +1,74 @@
+from functools import wraps
+from nose.tools import assert_raises
+from pyqs import events
+
+
+def clear_events_registry(fn):
+    """Clear the global events registry before each test."""
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        events.clear_events()
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+@clear_events_registry
+def test_register_event():
+    def print_pre_process(context):
+        print(context)
+
+    events.register_event("pre_process", print_pre_process)
+    events.get_events().pre_process.should.equal([print_pre_process])
+
+
+@clear_events_registry
+def test_register_multiple_same_events():
+    def print_pre_process(context):
+        print(context)
+
+    def print_numbers(context):
+        print(1 + 2)
+
+    events.register_event("pre_process", print_pre_process)
+    events.register_event("pre_process", print_numbers)
+    events.get_events().pre_process.should.equal([print_pre_process, print_numbers])
+
+
+@clear_events_registry
+def test_register_different_events():
+    def print_pre_process(context):
+        print(context)
+
+    def print_post_process(context):
+        print(context)
+
+    events.register_event("pre_process", print_pre_process)
+    events.register_event("post_process", print_post_process)
+    events.get_events().pre_process.should.equal([print_pre_process])
+    events.get_events().post_process.should.equal([print_post_process])
+
+
+@clear_events_registry
+def test_register_multiple_different_events():
+    def print_pre_process(context):
+        print(context)
+
+    def print_post_process(context):
+        print(context)
+
+    def print_numbers(context):
+        print(1 + 2)
+
+    events.register_event("pre_process", print_pre_process)
+    events.register_event("pre_process", print_numbers)
+    events.register_event("post_process", print_post_process)
+    events.register_event("post_process", print_numbers)
+    events.get_events().pre_process.should.equal([print_pre_process, print_numbers])
+    events.get_events().post_process.should.equal([print_post_process, print_numbers])
+
+
+@clear_events_registry
+def test_register_non_existent_event():
+    non_existent_event = "non_existent_event"
+    assert_raises(events.NoEventException, events.register_event, non_existent_event, lambda x: x)
